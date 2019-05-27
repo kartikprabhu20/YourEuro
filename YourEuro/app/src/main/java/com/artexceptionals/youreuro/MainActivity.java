344857f289624f03da63aa8d300360cadee4591f
avinashranjan1 <44359009+avinashranjan1@users.noreply.github.com>
2019-05-27 23:26:50 +0200

    NavigationBar

--- YourEuro/app/src/main/java/com/artexceptionals/youreuro/MainActivity.java
+++ YourEuro/app/src/main/java/com/artexceptionals/youreuro/MainActivity.java
@@ -1,8 +1,14 @@
 package com.artexceptionals.youreuro;
 
+
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -11,6 +17,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.artexceptionals.youreuro.adapter.CashRecordAdapter;
 
@@ -18,6 +25,9 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawerlay;
+    private ActionBarDrawerToggle abdt;
+
 
     @BindView(R.id.recent_history_rv)
     RecyclerView mRecentsRecyclerView;
@@ -25,16 +35,53 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.noRecords_tv)
     TextView noRecordsTextView;
 
+
     private MoneyControlManager moneyControlManager;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        drawerlay=(DrawerLayout)findViewById(R.id.drawerlay);
+        abdt=new ActionBarDrawerToggle(this,drawerlay,R.string.Open,R.string.Close);
+        abdt.setDrawerIndicatorEnabled(true);
+
+
+        drawerlay.addDrawerListener(abdt);
+        abdt.syncState();
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final NavigationView nav_View=(NavigationView)findViewById(R.id.nav_view);
+
+        nav_View.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+
+                if (id == R.id.home) {
+                    Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                } else if (id == R.id.statistics) {
+                    Toast.makeText(MainActivity.this, "Statistics", Toast.LENGTH_SHORT).show();
+                } else if (id == R.id.history) {
+                    startActivity(new Intent(MainActivity.this, HistoryActivity.class));
+
+
+                } else if (id == R.id.settings) {
+                    Toast.makeText(MainActivity.this, "Settings", Toast.LENGTH_SHORT).show();
+                } else if (id == R.id.aboutus) {
+                    Toast.makeText(MainActivity.this, "About Us", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
 
         Bundle pinBundle = getIntent().getExtras();
 
@@ -71,12 +118,16 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+
+
         int id = item.getItemId();
+
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
@@ -86,7 +137,7 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
 
-        return super.onOptionsItemSelected(item);
+        return abdt.onOptionsItemSelected(item)|| super.onOptionsItemSelected(item);
     }
 
 }
